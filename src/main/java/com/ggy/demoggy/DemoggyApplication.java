cfab49c5fa2876ff2d1575c12b2d1e937b506fc3
Guoguanyang <462870243@qq.com>
2021-01-02 22:50:33 +0800

    first commit

---
+++ src/main/java/com/ggy/demoggy/DemoggyApplication.java
@@ -0,0 +1,13 @@
+package com.ggy.demoggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoggyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoggyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggy/demoggy/DemoggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggy.demoggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoggyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
