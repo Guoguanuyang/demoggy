333098cd9117f58e57e43da3dfd8dbbac6d0dd09
Guoguanyang <462870243@qq.com>
2021-01-13 00:58:29 +0800

    add getUser

---
+++ src/main/java/com/ggy/demoggy/config/HttpAspect.java
@@ -0,0 +1,53 @@
+package com.ggy.demoggy.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpAspect {
+    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.ggy.demoggy.controller.LoginController.*(..))")
+    public void log() {
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        logger.info("url={}", request.getRequestURI());
+
+        //method
+        logger.info("method={}", request.getMethod());
+
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+
+        //method
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        //param
+        logger.info("args={}", joinPoint.getArgs());
+    }
+
+    @After("log()")
+    public void doAfter() {
+    }
+
+    @AfterReturning(returning = "obj", pointcut = "log()")
+    public void doAfterReturnig(Object obj) {
+        logger.info("reponse={}", obj);
+
+    }
+}
